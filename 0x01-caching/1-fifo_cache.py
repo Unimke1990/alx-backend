2c7de6a7cd4a038ca4d5e5ce2331795c0db852be
Unimke1990 <agimunimke1990@gmail.com>
2024-10-30 15:31:37 +0200

    added a new check for key in FIFO caching

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -1,37 +1,31 @@
 #!/usr/bin/env python3
+"""This module defines a class `FIFOCache`"""
 
-'''Task 1: FIFO caching
-'''
-
-
-from collections import OrderedDict
 from base_caching import BaseCaching
+from collections import deque
 
 
 class FIFOCache(BaseCaching):
-    '''A class `FIFOCache` that inherits from
-       `BaseCaching` and is a caching system.
-    '''
+    """A caching system"""
 
     def __init__(self):
+        """Initializes the class attributes."""
         super().__init__()
-        self.cache_data = OrderedDict()
+        self.fifo_queue = deque()
 
     def put(self, key, item):
-        '''assign to the dictionary `self.cache_data` the
-           `item` value for the key `key`
-        '''
+        """Adds an item in the cache."""
 
-        if key is None or item is None:
-            return
+        if key and item:
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS and\
+                    key not in self.cache_data:
+                discarded_key = self.fifo_queue.popleft()
+                del self.cache_data[discarded_key]
+                print("DISCARD: {}".format(discarded_key))
 
-        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-            first_key, _ = self.cache_data.popitem(last=False)
-            print(f"DISCARD: {first_key}")
-
-        self.cache_data[key] = item
+            self.cache_data[key] = item
+            self.fifo_queue.append(key)
 
     def get(self, key):
-        '''return the value in `self.cache_data` linked to `key`
-        '''
+        """Gets an item by key."""
         return self.cache_data.get(key, None)
