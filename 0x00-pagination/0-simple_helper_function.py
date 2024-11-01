772a47ee9daf6fb81472837d8631d1f6508381c1
Unimke1990 <132392244+Unimke1990@users.noreply.github.com>
2024-11-02 01:58:27 +0200

    function that returns a tuple of size two containing a start index and an end index

---
+++ 0x00-pagination/0-simple_helper_function.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""module defines the function index_range"""
+
+
+def index_range(page, page_size):
+    """returns turple of size two containing start and end indexes"""
+    start = (page - 1) * page_size
+    end = start * page_size
+    return (start, end)
